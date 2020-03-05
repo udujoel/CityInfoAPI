77381b687298091aa5186dafd9c54912e2e39efa
Joell <amamihechi.udu@decagon.dev>
2020-03-05 13:21:48 +0100

    Routing with keywords

--- CityInfoAPI/Controllers/CitiesController.cs
+++ CityInfoAPI/Controllers/CitiesController.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace CityInfoAPI.Controllers
 {
     [ApiController]
+    [Route("api/cities")]
     public class CitiesController : ControllerBase
     {
+
         public JsonResult GetCities()
         {
 
-            return new JsonResult(new List<object>(){new {id=1,Name="New York CIty"},new {id=2,Name="Antwar"}});
+            return new JsonResult(new List<object>() { new { id = 1, Name = "New York CIty" }, new { id = 2, Name = "Antwar" } });
         }
     }
 }
