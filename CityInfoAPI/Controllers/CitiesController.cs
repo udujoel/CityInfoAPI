c00ab63a01613bd56aa5ff44c380b10db7dc04c3
Joell <amamihechi.udu@decagon.dev>
2020-03-05 05:46:50 +0100

    getcities api configure

---
+++ CityInfoAPI/Controllers/CitiesController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfoAPI.Controllers
+{
+    [ApiController]
+    public class CitiesController : ControllerBase
+    {
+        public JsonResult GetCities()
+        {
+
+            return new JsonResult(new List<object>(){new {id=1,Name="New York CIty"},new {id=2,Name="Antwar"}});
+        }
+    }
+}
